971e9a402d7099fccdf0ce19e0232be48d1d583e
lawinduski <lawinduski195@gmail.com>
2026-09-20 20:10:49 +0300

    Update AdRotator.tsx

--- src/components/AdRotator.tsx
+++ src/components/AdRotator.tsx
@@ -118,7 +118,7 @@ export function AdRotator({
 
     body.style.overflow = 'hidden';
     body.style.position = 'fixed';
-    body.style.top = -${scrollY}px;
+    body.style.top = '-${scrollY}px';
     body.style.width = '100%';
 
     return () => {
