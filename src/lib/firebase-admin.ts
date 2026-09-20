14bbe41a9c6136c2614e08531403879bff4aa75a
lawinduski <lawinduski195@gmail.com>
2026-09-21 00:24:54 +0300

    Update firebase-admin.ts

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -5,7 +5,7 @@ const projectId = process.env.FIREBASE_ADMIN_PROJECT_ID;
 const clientEmail = process.env.FIREBASE_ADMIN_CLIENT_EMAIL;
 const privateKey = process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, '\n');
 
-if (!projectId  !clientEmail  !privateKey) {
+if (!projectId || !clientEmail || !privateKey) {
   throw new Error('Firebase Admin environment variables are missing.');
 }
 
